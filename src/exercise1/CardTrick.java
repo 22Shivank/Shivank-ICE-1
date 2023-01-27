774acf7c810b8e56421d1d3e7dba88de9b7a1fc4
22Shivank <shivankpanchhi222@gmail.com>
2023-01-26 23:59:39 -0500

    Shivank Panchhi ICE 26

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,4 +1,5 @@
 package exercise1;
+import java.util.Scanner;
 
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
@@ -11,35 +12,39 @@ package exercise1;
 public class CardTrick {
     
     public static void main(String[] args) {
-        
         Card[] hand = new Card[7];
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
-        }
+             int value = (int) (Math.random() * 13) + 1;
+            int suit = (int) (Math.random() * 4);
+            hand[i] = new Card(value, Card.SUITS[suit]);
 
-        // insert code to ask the user for Card value and suit, create their card
-        // and search the hand here. 
-        // Hint: You can ask for values 1 to 10, and then
-        //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
-        //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
-        // 
-        // Then loop through the cards in the array to see if there's a match.
+        }
+            Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter card value (1-13): ");
+        int value = scanner.nextInt();
+        System.out.println("Enter card suit (1-4): ");
+        int suit = scanner.nextInt();
+        Card userCard = new Card(value-1, Card.SUITS[suit-1]);
+
+        // Search the hand for the user's card
+        boolean found = false;
+        for (Card c : hand) {
+            if (c.equals(userCard)) {
+                found = true;
+                printInfo();
+            }
+        }
         
-        // If the guess is successful, print System.out.println("Congratulations, you guessed right!");.
+         if (found) {
+            printInfo();
+        } else {
+            System.out.println("Sorry, the card is not in the hand.");
+}
         
     }
 
-    /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
-     */
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
@@ -63,5 +68,7 @@ public class CardTrick {
         
     
     }
+
+    
 
 }
